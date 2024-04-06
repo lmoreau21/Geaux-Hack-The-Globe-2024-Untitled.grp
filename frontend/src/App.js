0d92d4a574110f33b2a966e235cb4bc12710eb03
lmoreau2021 <lmoreau2021@gmail.com>
2024-04-06 09:35:55 -0500

    abstraction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,95 +1,11 @@
 import React, { useState } from 'react';
-import { Container, TextField, Button, List, ListItem, ListItemText, Paper, Box } from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './NavBar';
-import axios from 'axios';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-
-function Chat() {
-  const [messages, setMessages] = useState([]);
-  const [inputText, setInputText] = useState('');
-  const [latestResponse, setLatestResponse] = useState('');
-
-  const sendMessage = async (e) => {
-    e.preventDefault();
-    if (!inputText.trim()) return;
-
-    // Update the chat history state before sending the request
-    const updatedMessages = [...messages, { role: "human", content: inputText }];
-    setMessages(updatedMessages);
-    setInputText('');
-    var source = 'gov_medicare';
-
-    try {
-      const response = await axios.post('http://127.0.0.1:8000/chatbot/', {
-        question: inputText,
-        chat_history: updatedMessages,
-        date_source: source
-      });
-
-      // Assuming the response is successful and in the expected format
-      if (response.data && response.data.latest_response) {
-        // Update both the chat history and the latest response
-        setMessages(response.data.chat_history);
-        setLatestResponse(response.data.latest_response);
-      }
-    } catch (error) {
-      console.error('Error sending message:', error);
-      // Handle error scenarios appropriately
-    }
-  };
-
-  return (
-    <Container maxWidth="sm">
-      <Paper style={{ maxHeight: 500, overflow: 'auto', marginTop: 20, marginBottom: 20, padding: '20px' }}>
-        <List>
-          {messages.map((message, index) => (
-            <ListItem key={index}>
-              {/* Render the message content or Markdown based on the role */}
-              {message.role === 'ai' ? (
-                <Paper elevation={3} style={{ marginTop: 20, padding: '15px' }}>
-                  <ReactMarkdown children={latestResponse} remarkPlugins={[remarkGfm]} />
-                </Paper>
-              ) : (
-                <ListItemText primary={message.content} secondary={message.role === 'human' ? 'You' : 'AI'} />
-              )}
-            </ListItem>
-          ))}
-        </List>
-        
-      </Paper>
-      <Box display="flex">
-        <TextField
-          variant="outlined"
-          fullWidth
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-          placeholder="Type your message here..."
-        />
-        <Button variant="contained" color="primary" endIcon={<SendIcon />} onClick={sendMessage}>
-          Send
-        </Button>
-      </Box>
-    </Container>
-  );
-}
-
-function Home() {
-  return <h1>Home Page</h1>;
-}
+import Chat from './ChatInterface';
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Chat />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
+    
+    
+    <Chat/>
   );
 }
 
--- frontend/src/ChatInterface.jsx
+++ frontend/src/ChatInterface.jsx
@@ -1,120 +1,124 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './ChatInterface.css'; 
+import React, { useState } from 'react';
+import { Container, TextField, Button, List, ListItem, ListItemText, Paper, Box, Drawer } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+import Divider from '@mui/material/Divider';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './NavBar';
 import axios from 'axios';
-import { FaChevronRight } from "react-icons/fa";
 import ReactMarkdown from 'react-markdown';
-import { StreamChat } from 'stream-chat'
+import remarkGfm from 'remark-gfm';
 
-function ChatInterface() {
-  const [messages, setMessages] = useState(() => {
-    // Load the chat history from localStorage when the component is mounted
-    const savedMessages = localStorage.getItem('chatHistory');
-    return savedMessages ? JSON.parse(savedMessages) : [];
-  });
 
+function Chat() {
+  const [messages, setMessages] = useState([]);
   const [inputText, setInputText] = useState('');
-  const messagesEndRef = useRef(null);
+  const [latestResponse, setLatestResponse] = useState('');
+  const [open, setOpen] = React.useState(false);
 
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
-  }
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
 
-  useEffect(scrollToBottom, [messages]);
-  
-  useEffect(() => {
-    // Save the chat history in localStorage whenever it changes
-    localStorage.setItem('chatHistory', JSON.stringify(messages));
-  }, [messages]);
-  
-  function roleSendMessage(role, content) {
-    // Add the role's message to the messages state
-    setMessages(prevMessages => [...prevMessages, { role, content }]);
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
-    // After the role sends a message, wait for 1 second and then get a response from the chatbot
-    setTimeout(() => {
-      console.log('Getting response from chatbot...');
-  
-      // Define the chat history and question
-      const chatHistory = messages;
-      const question = content;
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    if (!inputText.trim()) return;
 
-      // Define the data to be sent in the POST request
-      const data = {
-          "chat_history": chatHistory,
-          "question": question
-      };
-      console.log(data);
-  
-      axios.post('http://localhost:8000/chatbot/json_post/', data)
-          .then(response => {
-              // The response data is added to the messages state
-              console.log(response);
-              console.log('Response from chatbot: ', response['data']['latest_response']);
-              setMessages(response['data']['chat_history']);
-          })
-          .catch(error => {
-            
-            setMessages(prevMessages => [...prevMessages, { role: 'ai', content: 'Please try again later.' }]);
-            console.error('Error fetching data: ', error);
-          });
-     });
-  }
+    // Update the chat history state before sending the request
+    const updatedMessages = [...messages, { role: "human", content: inputText }];
+    setMessages(updatedMessages);
+    setInputText('');
+    var source = 'gov_medicare';
 
-  function handleSubmit(event) {
-      event.preventDefault();
-      roleSendMessage('user', inputText);
-      setInputText('');
-  }
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/chatbot/', {
+        question: inputText,
+        chat_history: updatedMessages,
+        date_source: source
+      });
+
+      // Assuming the response is successful and in the expected format
+      if (response.data && response.data.latest_response) {
+        // Update both the chat history and the latest response
+        setMessages(response.data.chat_history);
+        setLatestResponse(response.data.latest_response);
+      }
+    } catch (error) {
+      console.error('Error sending message:', error);
+      // Handle error scenarios appropriately
+    }
+  };
 
   return (
-    <div className="chat-interface">
-        <h1 className='title'>Chat Interface</h1>
-        <div className="chat-box">
-        <div className="messages">
-            <div className="message-list">
-            {messages.map((message, index) => {
-              if (message.role === 'ai') {
-                // For AI messages, use dangerouslySetInnerHTML to render HTML content
-                return (
-                  <div key={index} className='message-inner'>
-                    <div className='ai-icon' />
-                    <div className='message'>
-                      <h4 className='message-ai'>Health Bot</h4>
-                      <ReactMarkdown className='message-text'>{message.content}</ReactMarkdown>
-                      </div>
-                  </div>
-                );
-              } else {
-                // For user messages, render as plain text
-                return (
-                  <div key={index} className='message-inner'>
-                    <div className='user-icon' />
-                    <div className='message'>
-                      <h4 className='message-user'>You</h4>
-                      <p className='message-text'>{message.content}</p>
-                    </div>
-                  </div>
-                );
-              }
-            })}
-
-            <div ref={messagesEndRef} />
-            </div>
-            
-            <form className="message-form" onSubmit={handleSubmit}>
-              <textarea 
-                value={inputText}
-                onChange={event => setInputText(event.target.value)} 
-                placeholder="Type your message here..."
-              />
-               <button type="submit">
-               <FaChevronRight />
-                </button>
-            </form>
-      </div>
-      </div>
-    </div>
+    <Container maxWidth="sm">
+      <Button onClick={toggleDrawer(true)}>Open drawer</Button>
+    <Drawer open={open} onClose={toggleDrawer(false)}>
+      {DrawerList}
+    </Drawer>
+      <Paper style={{ maxHeight: 500, overflow: 'auto', marginTop: 20, marginBottom: 20, padding: '20px' }}>
+        <List>
+          {messages.map((message, index) => (
+            <ListItem key={index}>
+              {/* Render the message content or Markdown based on the role */}
+              {message.role === 'ai' ? (
+                <Paper elevation={3} style={{ marginTop: 20, padding: '15px' }}>
+                  <ReactMarkdown children={latestResponse} remarkPlugins={[remarkGfm]} />
+                </Paper>
+              ) : (
+                <ListItemText primary={message.content} secondary={message.role === 'human' ? 'You' : 'AI'} />
+              )}
+            </ListItem>
+          ))}
+        </List>
+        
+      </Paper>
+      <Box display="flex">
+        <TextField
+          variant="outlined"
+          fullWidth
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          placeholder="Type your message here..."
+        />
+        <Button variant="contained" color="primary" endIcon={<SendIcon />} onClick={sendMessage}>
+          Send
+        </Button>
+      </Box>
+    </Container>
   );
 }
 
-export default ChatInterface;
+export default Chat;
